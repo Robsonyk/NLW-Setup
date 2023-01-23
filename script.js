50a01f75c3cd5665303264b16cc4d03bc6146c20
Robsonyk <robsonyki@gmail.com>
2023-01-23 20:38:49 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change',save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+
+    const dayExists = nlwSetup.dayExists(today)
+    if (dayExists){
+      alert("Este dia ja  foi inserido")
+      return
+      
+    }
+    
+
+     alert('dia adicionado com sucesso')
+
+    nlwSetup.addDay(today)   
+}
+
+function save() {
+ localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+ 
+}
+
+
+//const data = {
+ // run:  ['01-01', '01-02', '01-04'],
+ // program:["01-03"],
+ // watch: ['01-02']
+//}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+ nlwSetup.load()
